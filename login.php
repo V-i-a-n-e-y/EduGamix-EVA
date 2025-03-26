f8ac8e48e5693d49aa232fb1a95d4553447c39c8
profebner <ebner.je@ixtapaluca.tecnm.mx>
2025-03-26 00:07:20 -0600

    se cambiaron usuario y password

--- login.php
+++ login.php
@@ -3,8 +3,10 @@ session_start();
 
 // Datos de la base de datos
 $servername = "localhost"; 
-$username = "u779086120_Bvianeyhm"; 
-$password = "W+>dWT1&1l"; 
+$username = "root"; 
+//$username = "u779086120_Bvianeyhm"; 
+$password = ""; 
+//$password = "W+>dWT1&1l"; 
 $dbname = "u779086120_edugamix"; 
 
 // Crear conexión 
